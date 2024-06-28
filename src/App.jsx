fa90dd430cdcb4838b0ee0a7c4f375d6cc16d2b9
Leandre <iamshemaleandre@gmail.com>
2024-06-28 11:27:25 +0200

    analytics organised

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import About from "./components/About"
@@ -20,6 +21,7 @@ function App() {
      <Clients/>
      <Contact/>
      <Footer/>
+     <Analytics />
     </>
   )
 }
